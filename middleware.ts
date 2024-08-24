5a0968b110ce9f60244955ff1ad474630f28cb42
priyam125 <sinha.priyam002@gmail.com>
2024-08-24 14:24:06 +0530

    fix

--- middleware.ts
+++ middleware.ts
@@ -16,3 +16,18 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
+
+
+// import { clerkMiddleware } from '@clerk/nextjs/server'
+
+// // Make sure that the `/api/webhooks/(.*)` route is not protected here
+// export default clerkMiddleware()
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// }
